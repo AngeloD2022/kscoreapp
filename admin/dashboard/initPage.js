f94e3f6ce60d2c7277c7e924bfcdb77953ee7454
Angelo Deluca <DeLucaA.2022@kenston.local>
2018-10-25 14:11:23 -0400

    cookies &php work 70%

--- admin/dashboard/initPage.js
+++ admin/dashboard/initPage.js
@@ -1,38 +1,28 @@
 var xhttp = new XMLHttpRequest();
-var url = "/script.php";
+var url = "/admin/script.php";
+var acct;
 function initializePage(){
-    var usr = findCookie("ksb_usr");
-    var pw = findCookie("ksb_pswd");
-    if(usr == "" || pw == ""){
-        document.location.href = "/admin";
-    }else{
-        if()
-    }
+
+    contactDB();
 }
 
-
-
-function findCookie(cname){
-    var cookies = decodeURIComponent(document.cookie);
-    var name = cname + "=";
-    var cookieSplit = cookies.split(';');
-    for(var i = 0; i < cookieSplit.length; i++){
-        var c = cookieSplit[i];
-        
-        while(String(c).charAt(0) == ' '){
-            c = c.substring(1);
-        }
-        if(String(c).indexOf(name) == 0){
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
 
 
 function contactDB(){
     xhttp.onreadystatechange = function() {
-        if()
+        if(this.readyState == 4 && this.status == 200){
+
+            if(this.responseText == "not found" || this.responseText == "disabled"){
+                console.log("not found or disabled");
+
+                document.location.href = "/admin";
+            }else{
+                var acc = JSON.parse(this.responseText);
+                var acct = acc[0];
+                document.getElementById("loginas").innerHTML = "Logged in as: "+ acct.name;
+            }
+
+        }
     }
         xhttp.open("GET", url, true);
         xhttp.send();
--- admin/script.php
+++ admin/script.php
@@ -1,8 +1,12 @@
 <?php
 
+
+if(htmlspecialchars($_COOKIE["ksb_usr"]) != null && htmlspecialchars($_COOKIE["ksb_pw"]) != null){
+	$username = htmlspecialchars($_COOKIE["ksb_usr"]);
+	$password = htmlspecialchars($_COOKIE["ksb_pw"]);
+}
 $username = isset($_GET["u"]) ? $_GET["u"] : "x";
 $password = isset($_GET["p"]) ? $_GET["p"] : "x";
-
 
 $conn = new mysqli("localhost", "root", null, "scoreboard");
 if($conn->connect_error){
@@ -10,7 +14,7 @@ if($conn->connect_error){
 }
 $passEncrypted = hash('sha256', $password);
 
-$sql = "SELECT * FROM users WHERE name='". $username ."' and passwordSHA256='". $passEncrypted . "'";
+$sql = "SELECT * FROM users WHERE name='". $username ."' and (passwordSHA256='". $passEncrypted . "' or passwordSHA256='".$_GET["p"]."')";
 
 $result = $conn->query($sql);
 
