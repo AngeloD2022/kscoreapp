dec504662de08f2cdf12aa782c93803258c1d0cd
Angelo Deluca <DeLucaA.2022@kenston.local>
2018-10-26 13:15:05 -0400

    Fixed bug with PHP/JS

--- admin/script.php
+++ admin/script.php
@@ -1,35 +1,46 @@
 <?php
+$username = "0";
+$password = "0";
 
-
-if(htmlspecialchars($_COOKIE["ksb_usr"]) != null && htmlspecialchars($_COOKIE["ksb_pw"]) != null){
-	$username = htmlspecialchars($_COOKIE["ksb_usr"]);
-	$password = htmlspecialchars($_COOKIE["ksb_pw"]);
+if (isset($_COOKIE["ksb_usr"]) && isset($_COOKIE["ksb_pswd"])) {
+	$username = $_COOKIE["ksb_usr"];
+	$password = $_COOKIE["ksb_pswd"];
 }
-$username = isset($_GET["u"]) ? $_GET["u"] : "x";
-$password = isset($_GET["p"]) ? $_GET["p"] : "x";
-
-$conn = new mysqli("localhost", "root", null, "scoreboard");
-if($conn->connect_error){
-	die("Failed: ". $conn->connect_error);
-}
-$passEncrypted = hash('sha256', $password);
-
-$sql = "SELECT * FROM users WHERE name='". $username ."' and (passwordSHA256='". $passEncrypted . "' or passwordSHA256='".$_GET["p"]."')";
-
-$result = $conn->query($sql);
+$username = isset($_GET["u"]) ? $_GET["u"] : $username;
+$password = isset($_GET["p"]) ? $_GET["p"] : $password;
 
 
-if($result->num_rows > 0){
-	while($row = $result->fetch_assoc()){
-		if($row["disabled"] == 1){
-			echo "disabled";
-		}else{
-			$resultArray[] = $row;
-			echo json_encode($resultArray);
-		}
-	}
-}else{
+//Checks if username or password == null
+if ($username == "0" || $password == "0") {
 	echo "not found";
+} else {
+	//contacts DB and outputs JSON or error message.
+	$conn = new mysqli("localhost", "root", null, "scoreboard");
+	if ($conn->connect_error) {
+		die("Failed: " . $conn->connect_error);
+	}
+	$passEncrypted = hash('sha256', $password);
+
+	$sql = "SELECT * FROM users WHERE name='" . $username . "' and (passwordSHA256='" . $passEncrypted . "' or passwordSHA256='" . $password . "')";
+
+	$result = $conn->query($sql);
+
+
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			if ($row["disabled"] == 1) {
+				echo "disabled Here";
+			} else {
+				$resultArray[] = $row;
+				echo json_encode($resultArray);
+			}
+		}
+	} else {
+		echo "not found HERE";
+	}
+	$conn->close();
+
 }
-$conn->close();
+
+
 ?>
