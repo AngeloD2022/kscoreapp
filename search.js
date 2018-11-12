9f7476ea4cb7651fd16f72b33d93be6dceac27c2
Angelo Deluca <DeLucaA.2022@kenston.local>
2018-11-12 13:56:44 -0500

    everything with the search dialog FINALLY works

--- search.js
+++ search.js
@@ -1,24 +1,25 @@
 var btn = document.querySelector("#sbtn");
 var box = document.querySelector("#sbox");
 
-document.addEventListener("click", function(event) {
-    
-    
-    if (event.currentTarget != box && event.currentTarget === btn && box.className === "SearchMenuDefault SearchMenuOpened"){//clicks on not the box and not the button (which means it's outside of the div) and while div is open
-       console.log("clicked on");
-    }else if(event.currentTarget === btn && box.className === "SearchMenuDefault SearchMenuOpened"){
-        
-    }
-    else{
-       
-    }
-    
+
+window.addEventListener("click", function (event) {
+
+if(event.target.nearestViewportElement != btn && event.srcElement != box && event.target.offsetParent != box && box.className.includes("SearchMenuOpened")){//(event.target != btndiv && event.target != box) && box.className.includes("SearchMenuOpened")){
+    console.log("Click off");
+    toggleSearchBox();
+}
+
+if(event.target.nearestViewportElement == btn){
+    console.log("Button Click");
+    toggleSearchBox();
+}
+
 
 });
 
 
-function toggleSearchBox(){
-    
+function toggleSearchBox() {
+
     btn.classList.toggle("sbClicked");
     box.classList.toggle("SearchMenuOpened");
 }
