cb461cfe7c6c4d636d6d245551dff79c5ee705c6
Angelo Deluca <DeLucaA.2022@kenston.local>
2018-12-05 14:12:18 -0500

    -

--- admin/app/core.js
+++ admin/app/core.js
@@ -1,32 +1,15 @@
 var request = new Array();
 var xhttp = new XMLHttpRequest();
 var ms = 0;
-
+//elements
+var kenstonScoreDisplay = document.getElementById("kscore");
 //scores
 var KenstonScore = 0;
 var GuestScore = 0;
 
 //misc
-updateGames();
+//readServer();
 
-function updateGames(){
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            var updated = JSON.parse(this.responseText);
-            for(var i = 0; i < updated.length; i++){
-                if(updated[i].id == gameId){
-                    KenstonScore = updated[i].homeScore;
-                    GuestScore = updated[i].oppScore;
-                    console.log("GT:Success\nHome:"+ KenstonScore+"\nGuest: "+GuestScore);
-                }
-            }
-
-        }
-    }
- xhttp.open("GET", "/data/events.php", true);
- xhttp.send();
-
-}
 
 
 function incrementScore(amount, team) {
@@ -66,19 +49,58 @@ function decrementScore(amount, team) {
 }
 
 function changeMisc() {
-
+    
 }
-
+var dLoad = document.getElementById("dLoad");
+var dLsvg = document.getElementById("determinateSVG");
+var lDiv = document.getElementById("loader");
 function sendTimer() {
-    //show timer icon
+    dLoad.style.color = "rgb(0, 195, 255)";
+    dLoad.style.strokeDashoffset = -313;
+    lDiv.className = "loaderShown";
+    console.log("start");
     var timer = setInterval(function () {
         ms++;
-        if (ms == 3000) {
-            sendToSrv();
+        dLoad.style.strokeDashoffset = parseInt(-313+((ms/300)*313));
+        kenstonScoreDisplay.innerHTML = ms;
+        if (ms == 300) {
+            readServer();
+            console.log("Done");
             //clear timer icon
+            ms = 0;
             clearInterval(timer);
         }
-    }, 1);
+    }, 10);
+    
+}
+
+function readServer(){
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var updated = JSON.parse(this.responseText);
+            for(var i = 0; i < updated.length; i++){
+                if(updated[i].id == gameId){
+                    KenstonScore = updated[i].homeScore;
+                    GuestScore = updated[i].oppScore;
+                    console.log("GT:Success\nHome:"+ KenstonScore+"\nGuest: "+GuestScore);
+                }
+            }
+            
+        }
+    }
+    xhttp.onloadstart = function(){
+        //show indeterminite
+        dLsvg.style.stroke = "rgb(255, 196, 87)";
+        
+    }
+    xhttp.onload = function(){
+        dLsvg.style.stroke = "rgb(0, 216, 11)";
+        setTimeout(function(){
+            lDiv.className = "loaderHidden";
+        }, 1000)
+    }
+    xhttp.open("GET", "/data/events.php", true);
+    xhttp.send();
 
 }
 
@@ -86,12 +108,5 @@ function sendToSrv() {
     for (var k in request) {
         console.log("POST: " + k + " || " + request[k]);
     }
-
-}
-
-xhttp.onloadstart = function(){
-    //show indeterminite
-}
-xhttp.onloadend = function(){
-    //hide indeterminite
+    
 }
