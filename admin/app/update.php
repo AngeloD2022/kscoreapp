f28e6a1605c0841f4ab686fbaeb6740f8e4bd936
Angelo DeLuca <angelo.deluca.2022@kenstonapps.org>
2018-12-09 16:29:57 -0500

    update.php now works 99% with Miscellaneous

--- admin/app/update.php
+++ admin/app/update.php
@@ -1,20 +1,20 @@
 <?php
     
     $jdata = file_get_contents("php://input");
-    $content[] = json_decode($jdata, true);
-    $gameId = $content[0]["id"];
+    $content = json_decode($jdata, true);
+    $gameId = $content["id"];
     require "globalassets/dbinit.php";
     require "globalassets/authentication.php";
     $authBack = authenticate();
     if(!isset($authBack)){
         $authBack = "x";
-    }
-    
-    else
+    }else
     {
-        foreach($content as $root){
-            echo $key."::".$val."";
-            if($key != "id" && $key != "uid"){
+        echo "\nuid:".$authBack["id"];
+        echo "\ngid:".$content["id"];
+        foreach($content as $key => $val){
+            if($key != "id" && $key != "uid" && $key != "misc"){
+                echo $key."::".$val."";
                 // $sql = "SELECT * from events where usrID=".$authBack["id"]." and id=".$event;
                 if(gettype($val) == "string"){
                     $sql = "UPDATE `events` SET `".$key."` = '".$val."' WHERE `events`.`id` = ".$gameId." and `events`.`usrID` = ".$authBack["id"]."";
@@ -25,17 +25,16 @@
                 if($key === "id" || $key === "uid"){
                     echo "I am an idiot and not supposed to be counting this as a change. I should stop\n";
                 }
-                if(isset($content[0]["misc"])){
-                    foreach($content[0]["misc"] as $key => $value){
-                        
-                    }
-                }
-                //if(!isset($result->num_rows)){
-                    //    echo "error_$key:". $key;
-                    //}
-            }else{}
+                
+            }
+        }
+        if(isset($content["misc"])){
+            $encoded = json_encode($content["misc"]);
+            $sql = "UPDATE `events` SET `misc` = '". $encoded ."' WHERE `events`.`id` = ".$gameId." and `events`.`usrID` = ".$authBack["id"]."";
+            $result = $conn->query($sql);
         }
     }
+
     
     
     
