0f612cf9aedf808678bf66179f0255c86ca27af5
Angelo Deluca <DeLucaA.2022@kenston.local>
2019-02-05 14:13:32 -0500

    Working on tSync for bb

--- app/bb-core.js
+++ app/bb-core.js
@@ -24,7 +24,7 @@ function fetchData() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.responseText);
             if (data['misc'] != null) {
-                changeQIndicator(!('quarter' in data['misc']) ? 0 : data['misc'].quarter);
+                changePIndicator(!('period' in data['misc']) ? 0 : data['misc'].period);
 
                 if (data['misc'].ballPosess == "home") {
                     document.getElementById("kball").style.display = "inline-block";
@@ -34,7 +34,7 @@ function fetchData() {
                     document.getElementById("gball").style.display = "inline-block";
                 }
             } else {
-                changeQIndicator(0);
+                changePIndicator(0);
                 updateDown(1);
             }
             document.getElementById("kscore").innerHTML = data["homeScore"] == null ? 0 : data["homeScore"];
@@ -46,30 +46,32 @@ function fetchData() {
 }
 
 
-var q1 = document.getElementById("q1");
-var q2 = document.getElementById("q2");
-var q3 = document.getElementById("q3");
-var q4 = document.getElementById("q4");
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+var p3 = document.getElementById("p3");
+var p4 = document.getElementById("p4");
 
 var downNo = document.getElementById("dNum");
 var downAfter = document.getElementById("numSuffix");
 
-var psqi = q1;
+var pspi = p1;
 
-function changeQIndicator(v) {
+function changePIndicator(v) {
 
-    psqi.className = "qInd";
+    pspi.className = "pInd";
     if (v == 1) {
-        q1.className = "qIndCurrent";
+        p1.className = "pIndCurrent";
     } else if (v == 2) {
-        q2.className = "qIndCurrent";
+        p2.className = "pIndCurrent";
     } else if (v == 3) {
-        q3.className = "qIndCurrent";
+        p3.className = "pIndCurrent";
     } else if (v == 4) {
-        q4.className = "qIndCurrent";
+        p4.className = "pIndCurrent";
     }
     if (v == null) {
-        psqi = document.getElementById("q1");
+        pspi = document.getElementById("p1");
+    }else if(v != 0){
+        pspi = document.getElementById("p"+v);
     }
 }
 
@@ -84,6 +86,8 @@ var seconds;
 var srvInitial;
 var srvTime;
 var tEnabled = false;
+
+
 var checkTime = setInterval(function () {
     txhttp.open("GET", "timerData.php?id=" + gid, true);
     txhttp.onreadystatechange = function () {
@@ -92,47 +96,61 @@ var checkTime = setInterval(function () {
             srvTime = reply["unix"];
             srvInitial = reply["startValue"];
             srvState = reply["currentState"];
+
+            
             if (reply["currentState"] == "S" || reply["currentState"] == "P" || reply["currentState"] == "E") {
-                if(tEnabled){
+                if (tEnabled) {
                     clearInterval(timer);
                     tEnabled = false;
                 }
+
                 seconds = srvInitial;
+
                 if (seconds < 0) {
+
                     timerdisplay.innerHTML = "00:00";
+
                 } else {
 
                     timerdisplay.innerHTML = (Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60)) + ":" + (seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60);
                 }
+
                 timerStateLbl.innerHTML = "&#9612;&#9612;";
                 timerStateLbl.style.color = "orange";
+
             } else if (reply["currentState"] == "R") {
-                if(!tEnabled){
+
+                if (!tEnabled) {
                     runTimer();
                 }
+
                 timerStateLbl.innerHTML = "&#9654;";
                 timerStateLbl.style.color = "green";
                 seconds = srvInitial;
             }
+            
         }
     }
     txhttp.send();
-}, 500);
+}, 1000);
+
 var timer;
-function runTimer(){
+
+
+function runTimer() {
 
     tEnabled = true;
     timer = setInterval(function () {
         var now = new Date().getTime();
         var difference = now - srvTime;
         seconds = srvInitial - Math.floor(difference / 1000);
-    
+
         if (seconds < 0) {
             timerdisplay.innerHTML = "00:00";
         } else {
-    
-            timerdisplay.innerHTML =  (Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60)) + ":" + (seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60);
+
+            timerdisplay.innerHTML = (Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60)) + ":" + (seconds % 60 < 10 ? "0" + seconds % 60 : seconds % 60);
         }
-    
+
     }, 200);
 }
